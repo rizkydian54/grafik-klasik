d45aa71f7d1e8fde763aec8c77ee0448ad3d0512
rizkydian54 <dianrizky584@gmail.com>
2019-11-01 17:33:38 +0700

    grafik

---
+++ app/src/main/java/com/example/grafik_klasik/MainActivity.java
@@ -0,0 +1,48 @@
+package com.example.grafik_klasik;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+import android.widget.Button;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.content.Context;
+import android.opengl.GLSurfaceView;
+
+public class MainActivity extends AppCompatActivity {
+    private Button tekan;
+    private GLSurfaceView grafik1;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        tekan=(Button) findViewById(R.id.button1);
+        grafik1= new HelloESSurfaceView (this);
+        setContentView(grafik1);
+
+}
+    @Override
+    protected void onPause() {
+    super.onPause();
+    grafik1.onPause();
+    }
+    @Override
+    protected void onResume() {
+    super.onResume();
+    grafik1.onResume();
+    }
+    class HelloESSurfaceView extends GLSurfaceView {
+    public HelloESSurfaceView (Context context) {
+    super(context);
+    setRenderer(new segitiga());
+    }
+    }
+    @Override
+    public boolean onCreateOptionsMenu (Menu menu) {
+
+        getMenuInflater().inflate(R.menu.activity_main, menu);
+        return true;
+    }
+    }
